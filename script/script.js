79f592543fe94d63c4e043f6b5566bb1145bf1cc
dacaroglu <dacaroglu@gmail.com>
2022-08-12 12:42:29 -0400

    final project

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ const playerBoard = document.querySelector('#playerTable')
 playerBoard.classList.add('selectable')
 const enemyBoard = document.querySelector('#enemyTable')
 const body = document.querySelector('body')
-
+const board = document.querySelector('.container')
 const playerTable = playerBoard.appendChild(document.createElement('table'))
 const enemyTable = enemyBoard.appendChild(document.createElement('table'))
 playerTable.classList.add('playerZone')
@@ -14,11 +14,14 @@ shipTable.classList.add('selectable')
 const shipTr = shipTable.appendChild(document.createElement('tr'))
 const output = document.querySelector('#output')
 const letters=['A','B','C','D','E','F','G','H','I','J','K','L']
-let empty,shot,hit // change it to the deep copy 
-const gamePhases = {0:'Menu',1:'New Game',2:'Play' ,3:'Game Over'}
-let phase = gamePhases[0]
+let miss={'p':[],'e':[]}
+let hit={'p':[],'e':[]} // change it to the deep copy 
+const gamePhases = {0:'Menu',1:'placement', 2:'play' ,3:'gameover'}
+let phase = 'newgame'
 let trgClass, trgInner
-
+let arrFilled=[]
+let turn = true
+let t =''
 // Ship status are: idle, placed, destroyed(destroyed phase comes with when the location array got empty due to move onto hit bar)
 let playerShips=
 {
@@ -48,7 +51,33 @@ size:2,
 location:[],
 status:'idle'}
 }
-let enemyShip = {...playerShips}
+let enemyShip = {
+    'Carrier'   :{
+    size:5,
+    location:[],
+    status:'idle'
+    },
+    
+    'Battleship':{
+    size:4,
+    location:[],
+    status:'idle'},
+    
+    'Cruiser'   :{
+    size:3,
+    location:[],
+    status:'idle'},
+    
+    'Submarine' :{
+    size:3,
+    location:[],
+    status:'idle'},
+    
+    'Destroyer' :{
+    size:2,
+    location:[],
+    status:'idle'}
+    }
 function drawTable(place)
 {   let zone='p'
     if(place.classList!='playerZone'){zone='e'}
@@ -68,9 +97,9 @@ function drawTable(place)
             // td.innerHTML=(i+'th row '+t+'th column')
             td.classList.add(pos,'cell',zone)
             td.setAttribute('id',zone+pos)
-
         }    
 }}
+
 function clean()
 {
     Object.keys(playerShips).forEach(e => {
@@ -84,75 +113,128 @@ function drawShips(playerShips)
     console.log('I have drawn the ships')
     for(let s in playerShips)
     {
-        
         const tdShipname = shipTr.appendChild(document.createElement('td'))
         tdShipname.innerHTML=`${s}`
-        tdShipname.classList=s
-        // const tdShipSize = shipTr.appendChild(document.createElement('td'))
-        // tdShipSize.innerHTML=`${playerShips[s].size}`
-        // tdShipSize.classList.add(s.toLowerCase())
-        // tdShipSize.classList.add('selectable')
-        //console.log(s+' '+playerShips[s])
+        tdShipname.classList.add('ship')
+        tdShipname.setAttribute('id',s)
     }
     
 }
-drawTable(playerTable)
-drawTable(enemyTable)
-drawShips(playerShips)
-phase=gamePhases[1]
-shipSelector()
-clean()
+newGame()
 function newGame()
 {
     drawTable(playerTable)
     drawTable(enemyTable)
     drawShips(playerShips)
-    phase=gamePhases[1]
-    shipSelector()
+    phase='menu'
+    // shipSelector()
     clean()
-}
-function shipSelector()
-{   
-    shipTable.addEventListener('click',function(e)
-    {   if(!shipTable.classList.contains('selectable')){return}
-        target = e.target
-        if(playerShips[target.classList].status=='placed'){return}
-        shipname = target.classList
-        shipSelected= true//to control if player has selected a ship to place
-        console.log(` You have selected ${shipname}`)
-        
-        shipTable.classList.remove('selectable')
-        
-    })
     boardSelector()
+    arrFilled=[]
 }
-function boardSelector()
-{   
-    
-    playerBoard.addEventListener('click',function(e)
-    {
-    if(!playerBoard.classList.contains('selectable')){return}
-    target = e.target
-//check if same cell selected
-    if(target.classList.contains('selected') ){console.log('you cant select same cell for direction');return}
-    target.classList.add('selected')
-    if(typeof shipname =='undefined'){return}
-    x = slicer(target.classList[0]).a
-    y = slicer(target.classList[0]).b
-    const loc = target.classList
-    console.log(` You have clicked ${loc}`)
-    let preLocation=playerShips[shipname].location
-//check if origin point entered. If entered proceed for direction
-    if(preLocation.length>0 &&( x !=slicer(preLocation[0]).a && y!=slicer(preLocation[0]).b )){console.log('mal misin amk ');return}
-    preLocation.push(loc[0])
 
-    alert('now please select direction you want to place your ship:')
-    if(preLocation.length>1)
-    {   shipPlacer(preLocation[0],preLocation[1],shipname,)
-        console.log(`complete rest of the cells if there is enough space, also the coordinates are: ${x} ${y}`);return
-    }
+function boardSelector()
+{   let tempArr=[]
+    board.addEventListener('click',function(e)
+    {   
+         target= e.target
+        if(target.classList.contains('disabled')){return}
+        if (phase=='newgame'){phase='menu'}
+        
+        if(phase=='menu')
+        {   tempArr=[]
+            if(!target.classList.contains('ship')){return}
+            
+            shipname = target.getAttribute('id')
+            console.log(shipname)
+            phase='placement'
+
+        }
+        if(phase =='placement')
+        {   
+            if(!target.classList.contains('cell')){return}
+            let x = target.classList[0]
+            console.log(shipname, x)
+            tempArr.push(x)
+            x= parseInt(tempArr[0])
+            let y = parseInt(tempArr[1])
+            if(tempArr.length>1){shipPlacer(x,y,shipname);phase='menu'}
+            let td = document.querySelector('#ships')
+            if (td.querySelectorAll('.ship').length<1){phase='play'}
+            
+        }
+        if ( phase == 'play'){
+            console.log('play')
+            if(turn && target.classList.contains('e')){return}
+            if(!turn && target.classList.contains('p')){return}
+            turn=!turn
+            let l = 'p'
+            let who
+            if(turn){l='e'}else if (!turn){l='p'}
+            console.log(target.classList)
+            if(target.classList.contains(l))
+            {  
+                let shot  = target
+                turn?who=enemyShip:who=playerShips
+                theGame(shot,who,l)
+                
+                console.log(turn)
+            }
+                
+            else{return}
+        }
+ })
+}
+function boardMarker(hit,miss)
+{   
+    // if(!hit['e'] || !hit['p'] || !miss['p'] || !miss['e']){return}
+    let arr=[hit,miss]
+    arr.forEach(s => {
+        s['e'].forEach( function (a){
+            let td = document.getElementById('e'+a)
+            td.classList.add('disabled')
+        })
+        s['p'].forEach( function (a){
+            let td = document.getElementById('p'+a)
+            td.classList.add('disabled')
+        })
+    });
     
-}   )}
+}
+function theGame(target,who,l)
+{   boardMarker(hit,miss)
+    console.log('im running')
+    let missed = true
+     // if(!target.classList.contains(t)){return}
+    let shot = target.classList[0]
+    // console.log(who)
+    console.log(l)
+    Object.keys(who).forEach(r =>{
+        let ship= who[r]
+        let loc = ship.location
+        console.log('ship & shot locations: '+loc+'  '+shot )
+        if(loc.includes(shot))
+        {    missed=!miss
+            ship.size-=1;
+            hit[l].push(shot);
+            target.classList.add('hit')
+            console.log('you hit me!!')
+            checkGameOver(who)
+            return
+        }
+    
+    })
+    if(missed){;miss[l].push(shot);target.classList.add('miss');return}
+    
+}
+//AI: get 2 int (0-9)+1, merge, place to the board, if hit chek adjacent, if hit run direction checker, if disbaled pick another random int
+function checkGameOver(who)
+{   let r=0
+    Object.keys(who).forEach(function (y){
+        r+=who[y].size
+    })
+    if(r==0){alert('Game Over!!');phase='gameover';return}else{return}
+}
 function directionChecker(x,y)
 {   let x1,x2,y1,y2
     y1=slicer(x).a
@@ -160,95 +242,80 @@ function directionChecker(x,y)
     y2=slicer(y).a
     x2=slicer(y).b
     
-    if(x1>x2){return -1}
-    else if (x1<x2){return +1}
-    else if(y1>y2){return -100}
-    else if(y1<y2){return +100}}
-    // if(x1>x2){console.log('<-');return -1}
-    // else if (x1<x2){console.log('->');return +1}
-    // else if(y1>y2){console.log('^');return -100}
-    // else if(y1<y2){console.log('v');return +100}}
+    if(x1>x2){console.log('<-');return -1}
+    else if (x1<x2){console.log('->');return +1}
+    else if(y1>y2){console.log('^');return -100}
+    else if(y1<y2){console.log('v');return +100}}
 
 //slicing the coordinates
 function slicer(x='')
-{x=x.toString();let a=x.slice(0,2);let b=x.slice(2,4);return {a,b}}
-
-function shipPlacer(x,y,ship={})
-{ 
-  ship = playerShips[ship]
-  size = ship.size
-  ship.location=[x]
-  
-  let findir = directionChecker(x,y)
-  x=parseInt(x)
-  y=parseInt(y)
-  //checking for enough space
-    for(i=0;i<size-1;i++)
-    {   a=parseInt(x)+findir
-        if(a>1010 || 1001> a >910 || a<101 || 201>a>110){ship.location=[];console.log('please select a valid direction');return;}
-        b= a<1000? 0 + a.toString():a.toString()
-        ship.location.push(b)
-        x+=findir
-        ship.status='placed'
+{   x=x<1000?'0'+x.toString():x.toString()
+    console.log('slicer is getting: '+x)
+    x=x.toString();let a=x.slice(0,2);let b=x.slice(2,4);return {a,b}}
+function shipPlacer(x,y,selectedShip)
+{   if(!x && !y && selectedShip){return}
+    let broken=false
+    ship = playerShips[selectedShip]
+    loc = ship.location
+    ship.location=[]
+    size= ship.size
+    dir = directionChecker(x,y)
     
+    for(i=0;i<size; i++)
+    {
+        let xStr = x<1000?'0'+x.toString():x.toString()
+            let td = document.getElementById('p'+xStr)
+            if(td==null){break}
+            arrFilled.push(x)
+            ship.location.push(xStr)
+            x+=dir
+            
+            
+            if(arrFilled.includes(x))
+            {   td.classList.remove('ship', selectedShip+i)
+                
+                broken=true;
+                console.log('this space is not available');
+                ship.location=[];
+                break
+            }else{
+                td.classList.add('ship', selectedShip+i)
+                document.getElementById(selectedShip).classList.remove('ship')
+            }
     }
-    // console.log(ship.location)
-    shipTable.classList.add('selectable')
-    Object.keys(playerShips).forEach(e => {
-        if(playerShips[e].status=='placed')
-        {
-           playerShips[e].location.forEach(function (t,z)
-           {
-            td = document.getElementById('p'+t)
-            td.classList.add('filled')
-            td.classList.add(e+z)
-            td.classList.remove('selected')
-           }) 
-        }
-    })
-}
-document.querySelector('#newGameButton').addEventListener('click',function(e){newGame();enemyShipPlacer()})
+    
+    ship.status='placed'
 
-     
+}
+document.querySelector('#newGameButton').addEventListener('click',function(e){location.reload()})
+
 function enemyShipPlacer()
-{   
+{   let arrFilled=[]
     document.querySelectorAll('.filled').forEach(a => {a.classList.remove('filled');})
-    Object.keys(enemyShip).forEach(l => {enemyShip[l].location=[];enemyShip[l].status='idle'})
     Object.keys(enemyShip).forEach(e => {
         let ship= enemyShip[e]
         let size = ship.size
-        let locs=randomLoc()
-        // while (document.getElementById('e'+locs[0]).classList.contains('filled') || document.getElementById('e'+locs[1]).classList.contains('filled')) {
-        //     document.querySelectorAll('.filled').forEach(a => {a.classList.remove('filled');})
-        //     randomLoc()
-        //     return enemyShipPlacer()
-            
-        // }
+        ship.location=[]
+        let locs=randomLoc(size)
         let x=locs[0]
-        let y=locs[1]
-     // console.log('ship builders location: '+x+' and '+y)
-     ship.location=[x]
-     document.getElementById('e'+x).classList.add('filled')
-    let findir = directionChecker(x,y)
-    x=parseInt(x)
-    y=parseInt(y)
-    console.log(ship)
-    //checking for enough space
-    for(i=0;i<size-1;i++)
-    {   let a=parseInt(x)+findir//this is our new b value(for the direction this for loop goes until it places everything)
-        if(a>1010 || 1001> a >910 || a<101 || 201>a>110 ||a%100==0 ||a%100<=0 || a%100>=10){ship.location=[];enemyShipPlacer()}
-        let b= a<1000? 0 + a.toString():a.toString()
-        // document.getElementById('e'+b).classList.add('filled')
-        ship.location.push(b)
-        x+=findir
+        let dir = locs[1]
+        console.log('IMPORT: Origin: '+x+' Direction: '+dir)
+        for(i=0;i<size;i++)
+        {   let xStr=x<1000?'0'+x.toString():x.toString()
+            if(arrFilled.includes(x)){return enemyShipPlacer()}
+            else{
+            arrFilled.push(x)
+            ship.location.push(xStr)
+            
+            x+=dir
+            }
+        }
         ship.status='placed'
-    }
+        console.log(ship.location)
 })
-overlapChecker()
-// Object.keys(enemyShip).forEach(d=>{console.log(enemyShip[d].location)})
 }
 //worst way to check duplicates inside of dictionary, needs to be improved ,  IT DOESNT EVEN WORK..
-function overlapChecker(x,y,ship)
+function overlapChecker()
 {   
     Object.keys(enemyShip).forEach(e => {
         ship = enemyShip[e]
@@ -258,67 +325,53 @@ function overlapChecker(x,y,ship)
             
             Object.keys(enemyShip).forEach(g =>
                 {
-                    
                     if(enemyShip[g].location.includes(t) && e!=g && (t.length + enemyShip[g].location.length > 2 )){console.log(e,g+'\n'+ 'locations overlapped: ' +enemyShip[g].location+' and '+t); enemyShipPlacer()}    
                 })
-        
         })
     });
-    
 }
-function enemyShipShow()
-{   
-    Object.keys(enemyShip).forEach(e => {
-        if(enemyShip[e].status=='placed')
+function shipShow(who)
+{   a= who==playerShips?'p':'e'
+    Object.keys(who).forEach(e => {
+        if(who[e].status=='placed')
         {
-        enemyShip[e].location.forEach(function (t,z)
+        who[e].location.forEach(function (t)
             {
-                tde =  document.getElementById('e'+t.toString())
+                console.log(a+t.toString())
+                tde =  document.getElementById(a+t.toString())
                 console.log(tde.classList+'   '+t)
-                // tde.classList.add('filled')
-                tde.classList.add(e+z)
+                tde.classList.add('filled')
+                // tde.classList.add(e+z)
                 console.log(`placement in ${t}`)
                 tde.classList.remove('selected')
             }) 
         }
     })
 }
-function randomLoc()
+function randomLoc(size=5)
 { 
-    x=Math.round(Math.floor(Math.random()*1000)/100)*100+Math.floor(Math.random()*10);
-    locPic=[x+1,x-1,x+100,x-100]
-    y=locPic[Math.floor(Math.random()*4)]
-    if(x%100<1 ||x%100>10 || x<101 || x>1010)
-    { 
-        x=0
-        y=0
-        randomLoc()
-    }else if(y%100<1 ||y%100>10 || y<101 || y>1010) 
-    {   x=0
-        y=0
-        randomLoc()
-    }
-    else if(x!=undefined && y!=undefined)
-    {       
-            x=x<1000 ? '0'+x.toString() : x.toString()
-            y=y<1000 ? '0'+y.toString() : y.toString()
-            // console.log(x+'   '+y)
-            
-    }
-    else{randomLoc()}
-    arr=[x, y]
-    return arr  
+    let x=Math.round(Math.floor(Math.random()*900)/100)*100+Math.floor(Math.random()*9+101);
+    
+    let locPic=[1,-1,100,-100]
+    let dir=locPic[Math.floor(Math.random()*4)]
+    
+    let limit= dir*(size-1)+x
+    console.log('GENERATOR: origin: '+x+' GENERATOR: Direction: '+dir+' Limit is: '+limit )
+    if(limit%100<1 || limit%100>10 || limit<101 || limit>1010){return randomLoc()}
+    
+    let arr=[x, dir]
+    if(arr[0]<101){randomLoc()}
+    return arr
 }
 function shipOverlapChecker(who={})
 {
     Object.keys(who).forEach(e=>{
         ship=who[e]
         ship.location.forEach(t => {
-            
         });
-
     })
 }
+const otoRun = setTimeout(enemyShipPlacer(), 20000);
 
 
 
